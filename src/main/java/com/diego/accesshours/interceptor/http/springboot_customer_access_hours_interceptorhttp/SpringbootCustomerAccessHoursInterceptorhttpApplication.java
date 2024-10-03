26719f76b3095fecfc6a67450e60c35ca897b47a
Diego Fernando Guerrero Perez <diego.guerrero9612@gmail.com>
2024-10-02 19:53:47 -0500

    primer commit del proyecto, ya esta configurado los metodos del controlador y los metodos prehandre y post handre del interceptor

---
+++ src/main/java/com/diego/accesshours/interceptor/http/springboot_customer_access_hours_interceptorhttp/SpringbootCustomerAccessHoursInterceptorhttpApplication.java
@@ -0,0 +1,13 @@
+package com.diego.accesshours.interceptor.http.springboot_customer_access_hours_interceptorhttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCustomerAccessHoursInterceptorhttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCustomerAccessHoursInterceptorhttpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diego/accesshours/interceptor/http/springboot_customer_access_hours_interceptorhttp/SpringbootCustomerAccessHoursInterceptorhttpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.accesshours.interceptor.http.springboot_customer_access_hours_interceptorhttp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCustomerAccessHoursInterceptorhttpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
